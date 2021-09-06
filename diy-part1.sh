4ed3ba5c88180898b81032da855a0c63fe0ee9a3
ShuiHuo <1365143958@qq.com>
2021-09-06 11:22:10 +0800

    修改文件

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >> feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/small.git' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small.git' >> feeds.conf.default
